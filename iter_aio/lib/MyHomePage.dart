605644ea89f35dd52422a1244d858be44fd65d05
Ayush Kejariwal <kejayush29@gmail.com>
2020-05-13 21:11:27 +0530

    initial commit

---
+++ iter_aio/lib/MyHomePage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:iteraio/data.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            RaisedButton(onPressed: (){
+              print('loading...');
+//              getData();
+            },
+              child: Text('Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ iter_aio/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:iteraio/MyHomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ITER-AIO',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'ITER-AIO'),
+    );
+  }
+}
